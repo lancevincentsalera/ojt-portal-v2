2816d147c8a36dc993751fcfb35f38e7c61dfd28
Lance Vincent A. Salera <lancevincent.salera@cit.edu>
2024-10-18 21:28:00 +0800

    initial training plan creation functionality done

--- src/Components/Supervisor/Modals/TrainingPlans/controller/CreateTrainingPlanModalController.js
+++ src/Components/Supervisor/Modals/TrainingPlans/controller/CreateTrainingPlanModalController.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import CreateTrainingPlanModalView from "../view/CreateTrainingPlanModalView";
 import { trainingPlanModel } from "../model/CreateTrainingPlanModalModel";
 import axios from "axios";
+import LoadingModal from "../../../../Common/Modals/LoadingModal";
+import OkayModal from "../../../../Common/Modals/OkayModal";
+import ErrorModal from "../../../../Common/Modals/ErrorModal";
+import PromptModal from "../../../../Common/Modals/PromptModal";
+import { useAuth } from "../../../../Common/AuthContext";
 
 const CreateTrainingPlanModalController = ({
   showModal,
@@ -9,14 +14,33 @@ const CreateTrainingPlanModalController = ({
 }) => {
   const [trainingPlan, setTrainingPlan] = useState(trainingPlanModel);
   const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+  const { userInfo } = useAuth();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [isPromptOpen, setIsPromptOpen] = useState(false);
 
   const handleCreateTrainingPlanAction = async () => {
+    setIsSubmitting(true);
     try {
       const url = apiBaseUrl + "/training/plans";
+      trainingPlan.mentorId = userInfo.user.id;
+      console.log(trainingPlan);
       const response = await axios.post(url, trainingPlan);
-      console.log(response);
+
+      if (response.status === 201) {
+        setIsSuccess(true);
+      } else {
+        setIsError(true);
+        setErrorMessage("Error creating training plan.");
+      }
     } catch (error) {
       console.error(error);
+      setIsError(true);
+      setErrorMessage("Error creating training plan: ", error);
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
@@ -68,15 +92,47 @@ const CreateTrainingPlanModalController = ({
     });
   };
 
+  const handleCreateTrainingPlan = () => {
+    setIsPromptOpen(true);
+  };
+
+  const handleConfirm = async () => {
+    setIsPromptOpen(false);
+    await handleCreateTrainingPlanAction();
+  };
+
   return (
-    <CreateTrainingPlanModalView
-      showModal={showModal}
-      handleModalAction={handleModalAction}
-      handleChange={handleChange}
-      handleTaskChange={handleTaskChange}
-      handleTechStackAndSkillChange={handleTechStackAndSkillChange}
-      handleCreateTrainingPlanAction={handleCreateTrainingPlanAction}
-    />
+    <>
+      <CreateTrainingPlanModalView
+        showModal={showModal}
+        handleModalAction={handleModalAction}
+        handleChange={handleChange}
+        handleCreateTrainingPlan={handleCreateTrainingPlan}
+      />
+      <LoadingModal open={isSubmitting} />
+
+      <OkayModal
+        open={isSuccess}
+        onClose={() => {
+          setIsSuccess(false);
+          window.location.reload();
+        }}
+        message="Training plan created successfully!"
+      />
+
+      <ErrorModal
+        open={isError}
+        onClose={() => setIsError(false)}
+        errorMessage={errorMessage}
+      />
+
+      <PromptModal
+        open={isPromptOpen}
+        onClose={() => setIsPromptOpen(false)}
+        onConfirm={handleConfirm}
+        message="Are you sure you want to submit create this training plan?"
+      />
+    </>
   );
 };
 
--- src/Components/Supervisor/Modals/TrainingPlans/view/CreateTrainingPlanModalView.js
+++ src/Components/Supervisor/Modals/TrainingPlans/view/CreateTrainingPlanModalView.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-const CreateTrainingPlanModalView = ({ showModal, handleModalAction }) => {
+const CreateTrainingPlanModalView = ({
+  showModal,
+  handleModalAction,
+  handleChange,
+  handleCreateTrainingPlan,
+}) => {
   return (
     <>
       <div className="modal-overlay" onClick={handleModalAction}></div>
@@ -12,23 +17,30 @@ const CreateTrainingPlanModalView = ({ showModal, handleModalAction }) => {
               &times;
             </span>
           </div>
-          <form className="modal-form no-subh">
-            <input type="text" id="title" name="title" placeholder="Title" />
+          <form
+            className="modal-form no-subh"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleCreateTrainingPlan();
+            }}
+          >
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              onChange={handleChange}
+            />
             <textarea
               type="text"
               id="description"
               name="description"
               placeholder="Description"
               className="large-textarea"
+              required
+              onChange={handleChange}
             />
-            <div className="tapad">
-              <label htmlFor="startDate">Start Date</label>
-              <label htmlFor="endDate">End Date</label>
-            </div>
-            <div className="tapad">
-              <input name="startDate" className="startDate" type="date" />
-              <input name="endDate" className="endDate" type="date" />
-            </div>
             <div className="button-group double">
               <button
                 type="button"
